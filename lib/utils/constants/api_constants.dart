0588d5235bb5e7f1b51a62f1a89d827cf5ac6bde
adefaze <adefaze@gmail.com>
2023-12-16 20:07:49 +0100

    added device utitlies

---
+++ lib/utils/constants/api_constants.dart
@@ -0,0 +1,8 @@
+/* -- LIST OF Constants used in APIs -- */
+
+class AppSecretKeys {
+  AppSecretKeys._();
+
+  static const String appSecretAPIKey =
+      "cwt_live_b2da6ds3df3e785v8ddc59198f7615ba";
+}
