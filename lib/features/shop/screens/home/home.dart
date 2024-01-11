8536040227800d3141f1f9dced81897422019fa1
adefaze <adefaze@gmail.com>
2024-01-11 22:17:39 +0100

    added curved edges container

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,3 +1,5 @@
+import 'package:ecommer_store/common/widgets/custom_shapes/container/circle_container.dart';
+import 'package:ecommer_store/common/widgets/custom_shapes/custom_edges/curved_edges.dart';
 import 'package:ecommer_store/utils/constants/colors.dart';
 import 'package:flutter/material.dart';
 
@@ -10,9 +12,33 @@ class HomeScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Container(
-              color: TAppColors.primary,
-              padding: const EdgeInsets.all(0),
+            ClipPath(
+              clipper: TCustomCurvedEdges(),
+              child: Container(
+                color: TAppColors.primary,
+                padding: const EdgeInsets.all(0),
+                child: SizedBox(
+                  height: 400,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        top: -150,
+                        right: -250,
+                        child: TCirlcularContainer(
+                            backgroundColor:
+                                TAppColors.textWhite.withOpacity(0.1)),
+                      ),
+                      Positioned(
+                        top: 100,
+                        right: -300,
+                        child: TCirlcularContainer(
+                            backgroundColor:
+                                TAppColors.textWhite.withOpacity(0.1)),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
           ],
         ),
